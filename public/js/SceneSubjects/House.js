e1a3a07ca05b25292a1a52dd43a83b63586d705f
Suraksha Motilal <surakshamotilal@gmail.com>
2021-05-23 01:02:07 +0200

    Changed everything to es6 classes. Working perfectly.

--- public/js/SceneSubjects/House.js
+++ public/js/SceneSubjects/House.js
@@ -1,15 +1,18 @@
-function House(scene) {
-  //load house model form blender file
+class House {
+  constructor(scene) {
+    //load house model form blender file
     const loader = new THREE.GLTFLoader();
-        loader.setPath('../models/');
-        loader.load('Aftermath_Map.glb', (gltf) => {
-          gltf.scene.traverse(c => {
-            c.castShadow = true;
-          });
-          scene.add(gltf.scene);
-        });
-	this.update = function(time) {
-       //do nothing
-	}
+    loader.setPath('../models/');
+    loader.load('Aftermath_Map.glb', (gltf) => {
+      gltf.scene.traverse(c => {
+        c.castShadow = true;
+      });
+      scene.add(gltf.scene);
+    });
 
+    this.update = function (time) {
+      //do nothing
+    };
+
+  }
 }
