22c1b04eb634efdfd8de2a7b4b48278f239559ed
Kiyolin Perumal <2115440@students.wits.ac.za>
2021-05-23 02:09:54 +0200

    Created Time class

---
+++ public/js/Time.js
@@ -0,0 +1,17 @@
+class Time {
+    constructor(){
+        this.total_run_time = 0;
+        this.clock = new THREE.Clock();
+    }
+
+
+    getDelta(){
+        return this.clock.getDelta();
+    }
+
+
+
+    
+
+
+}
