c16e2a32b975b15d8d8e041c3b223ed35eacd72e
Suraksha Motilal <surakshamotilal@gmail.com>
2021-05-25 17:22:48 +0200

    Animation working but not changing per movement.

--- public/js/SceneSubjects/characters/MainChar.js
+++ public/js/SceneSubjects/characters/MainChar.js
@@ -1,80 +1,55 @@
-
 class MainChar extends THREE.Object3D {
 	constructor() {
-		//load a model asnd animate it
-		//TODO!
 		super();
-	   this.object = new THREE.Object3D();
+	    this.object = new THREE.Object3D();
+	    this.clock=new THREE.Clock();
+		//this.object.rotateOnAxis( new THREE.Vector3(0,1,0), -Math.PI);
 		this.object.position.set(0, 1, 50);
-		this.object.scale.x=5;
-		this.object.scale.y=20;
-		this.object.scale.z=5;
-		const loader = new FBXLoader();
-		loader.setPath('../../models/characters/');
-		loader.load('Douglas.fbx', (fbx) => {
-		 
-		  fbx.scene.traverse(c => {
-			c.castShadow = true;
-		  });
-		  this.object.add(fbx);
-		});
+
+		//change the below to 8 to scale him to the correct scale
+		this.object.scale.x=8;
+		this.object.scale.y=8;
+		this.object.scale.z=8;
+		this.moveDistance = 0.5 ; 
+		this.mixers=[]
+
+
+		
+	//save keyboard bindings
+	//this.keyboard = new THREEx.KeyboardState();
+	
+		const loader = new THREE.GLTFLoader();
+   		loader.setPath('../../models/characters/');
+    
+  		var gltf = loader.load('walk.glb', (gltf) => {
+			gltf.scene.traverse(c => {
+				c.castShadow = true;
+				
+			});
+			this.idleMixer = new THREE.AnimationMixer( gltf.scene );
+			this.idleMixer.timeScale=1;
+			this.mixers.push(this.idleMixer)
+			var idle = this.idleMixer.clipAction( gltf.animations[ 0 ] );
+			idle.play();
+
+			this.object.add( gltf.scene );
+    	});
 	
 
-		//save keyboard bindings
-		this.keyboard = new THREEx.KeyboardState();
-		/*
-		//creating a box (need to change it to a character with animations)
-		const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-		const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-		this.object = new THREE.Mesh( geometry, material );
-		
-		this.object.scale.x=5;
-		this.object.scale.y=10;
-		this.object.scale.z=5;
-			//starting position for character
-		this.object.position.set(0, 10, 50);
-
-		*/
-		
 	
-
 		
 		this.update = function (time) {
 
-			//MOVEMENT OF BOX
-
-			//speed
-			var moveDistance = 0.5 ; 
-			var rotateAngle = Math.PI / 2 * 0.05;   
-			
-			// move forwards/backwards/left/right
-			if ( this.keyboard.pressed("W") ){
-				this.object.translateZ( -moveDistance );
-		    }
-			if (  this.keyboard.pressed("S") ){
-				this.object.translateZ(  moveDistance );
-			}
-			if (  this.keyboard.pressed("A") ){
-				this.object.translateX( -moveDistance );
-			}
-			if (  this.keyboard.pressed("D") ){
-				this.object.translateX( moveDistance );
+			//animation
+			if ( this.idleMixer ){
+			 this.idleMixer.update( this.clock.getDelta() );
 			}
 
+
+			//var rotateAngle = Math.PI / 2 * 0.05;   
 			// move forwards/backwards/left/right
-			if ( this.keyboard.pressed("up") ){
-				this.object.translateZ( -moveDistance );
-			}
-			if (  this.keyboard.pressed("down") ){
-				this.object.translateZ(  moveDistance );
-			}
-			if (  this.keyboard.pressed("left") ){
-				this.object.translateX( -moveDistance );
-			}
-			if (  this.keyboard.pressed("right") ){
-				this.object.translateX( moveDistance );
-			}
-			
+
+	
 		
 			// FOR CAMERA ROTATIONS
 			//this.object.rotateOnAxis( new THREE.Vector3(0,1,0), -rotateAngle);
@@ -82,11 +57,7 @@ class MainChar extends THREE.Object3D {
 			//var rotation_matrix = new THREE.Matrix4().identity();
 			
 			
-			if (  this.keyboard.pressed("Z") )
-			{
-				this.object.position.set(0, 1, 50);
-				this.object.rotation.set(0,0,0);
-			}
+		
 				
 			/*
 			// global coordinates
@@ -102,4 +73,31 @@ class MainChar extends THREE.Object3D {
 
 		};
 	}
+	/*
+	performMovement(){
+		var object=this.scene.getObjectByName('Douglas');
+		if (keyboardManager.movements.left){
+			object.translateX(-this.moveDistance);
+		}
+		if (keyboardManager.movements.right){
+			object.translateX(this.moveDistance);   
+		}
+		if (keyboardManager.movements.backward){
+			object.translateZ(this.moveDistance);
+		}
+		if (keyboardManager.movements.forward){
+			object.translateZ(-this.moveDistance);
+		}
+	}
+	*/
+	setName(name){
+		this.object.name=name;
+	}
+
+
+	resetChar(){
+		this.object.position.set(0, 1, 50);
+		this.object.rotation.set(0,0,0);
+	}
+
 }
