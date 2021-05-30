8813a6d18d4976920fb8ce0f3eac6610fa33bc7e
Fezile Mahlangu <mahlangufezile.3@gmail.com>
2021-05-30 02:39:00 +0200

    doors

--- public/js/SceneSubjects/objects/Door.js
+++ public/js/SceneSubjects/objects/Door.js
@@ -8,21 +8,28 @@ export class Door extends THREE.Object3D {
         super();
         this.object = new THREE.Object3D();
 
+
         this.clock = new THREE.Clock();
         const loader = new GLTFLoader();
         loader.setPath('../../models/');
-        this.stop = true;
+        this.open = true; //open door animation
+
+        this.startTime=0;
+
         var gltf = loader.load('testdoor.glb', (gltf) => {
             gltf.scene.traverse(c => {
                 c.castShadow = true;
 
             });
+
+            //scale door 
             this.object.scale.x = 0.271;
             this.object.scale.y = 0.271;
             this.object.scale.z = 0.271;
            
+            //play animation 
             this.idleMixer = new THREE.AnimationMixer(gltf.scene);
-            this.idleMixer.timeScale = 0.2;
+            this.idleMixer.timeScale = 0.2; //speed of animation
             this.idle = this.idleMixer.clipAction(gltf.animations[0]);
            // this.idle.play();
 
@@ -43,23 +50,44 @@ export class Door extends THREE.Object3D {
    
     update(time) {
 
-        console.log(time);
+        // console.log(time);
 
-        if (this.stop == false) {
+        if (this.open == true) { //animate
             if (this.idleMixer) {
                 this.idleMixer.update(this.clock.getDelta());
             }
         }
 
+        // if (keyboardManager.wasPressed('E')){
+        //     this.open=true;
+        //     this.startTime=time;
+        //     console.log(this.startTime);
+        //     // console.log("waspressed E");
+        //     this.idle.play();
+            
+        // }
+        // // console.log("timer");
+        // console.log(time);
+        // if(time==this.startTime+5){
+        //     this.idleMixer.stopAllAction();
+        //     this.open=false;
+        // }
+
         if (keyboardManager.wasPressed('E')) {
            
-            if (this.stop == true) {
+            if (this.open == false) { // animate
+
+                //this.startTime=time;
                 this.idle.play();
-                this.stop = false;
+                // if(this.startTime==this.startTime+1){
+                //     this.idleMixer.stopAllAction();//stop animation after start time+3
+                //     this.open = false;
+                // }
+                this.open = true;
             }
             else {
-                this.idleMixer.stopAllAction();
-                this.stop = true;
+                this.idleMixer.stopAllAction(); //stop animation
+                this.open = false;
             }
         }
     }
